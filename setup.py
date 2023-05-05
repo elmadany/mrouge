09b69e559369aa40ffabba66742548a446e9373c
AbdelRahim A. Elmadany <elmadany@gmail.com>
2023-05-05 14:29:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rouge_score",
+    name="mrouge",
     author="Google LLC",
     description="Multilingual ROUGE score based on Google rouge_score and xl-sum",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['rouge_score'],
+    packages=['mrouge'],
     package_dir = {'rouge_score':''},
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,8 +20,6 @@ setuptools.setup(
         "nltk",
         "numpy",
         "six>=1.14.0",
-        "git+https://github.com/otuncelli/turkish-stemmer-python",
-        "git+https://github.com/abhik1505040/bengali-stemmer",
         "pythainlp",
         "pyonmttok",
         "jieba",
